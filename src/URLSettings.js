bef294750af8034d0da9debb8c6fbbaf8f79b261
cvamsik <vamsi.chakravarula@gmail.com>
2019-12-05 01:52:59 +0000

    front

---
+++ src/URLSettings.js
@@ -0,0 +1,4 @@
+// export const ROOT_URL = "https://fconnect-backend.herokuapp.com";
+export const ROOT_URL = 'http://3.87.219.249:3001'
+export const FLASK_URL = 'http://3.87.219.249:3200'
+export const States = ['California', 'Washington', 'Texas']
